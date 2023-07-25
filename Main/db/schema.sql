38ad29aeff344d391c9bfef6aeb8b0dc5f59b5ed
nisha4r <nisha.4r@gmail.com>
2023-07-24 21:28:11 -0600

    adding constraints

--- Main/db/schema.sql
+++ Main/db/schema.sql
@@ -16,7 +16,8 @@ CREATE TABLE role (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INTEGER
+    department_id INTEGER,
+    CONSTRAINT foreignkey_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
@@ -25,5 +26,6 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER,
     manager_id INTEGER REFERENCES employee,
+    CONSTRAINT foreignkey_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL
 
-)
+);
