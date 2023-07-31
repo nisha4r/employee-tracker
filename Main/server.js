35baf82c09bb7d2c61a07c1e5efc1d95691fcd67
nisha4r <nisha.4r@gmail.com>
2023-07-30 20:21:09 -0600

    Adding prompt method

--- Main/server.js
+++ Main/server.js
@@ -1,5 +1,7 @@
 //import express
 const express = require('express');
+const consoleTable = require('console.table');
+const inquirer = require('inquirer');
 
 // Import and require mysql2
 const mysql = require('mysql2');
@@ -12,12 +14,39 @@ app.use(express.json());
 
 // Connect to database
 const db = mysql.createConnection(
-  {
-    host: 'localhost',
-    // MySQL username,
-    user: 'root',
-    password: '',
-    database: 'employeetracker_db'
-  },
-  console.log(`Connected to the employeetracker_db database.`)
+    {
+        host: 'localhost',
+        // MySQL username,
+        user: 'root',
+        password: '',
+        database: 'employeetracker_db'
+    },
+    console.log(`Connected to the employeetracker_db database.`)
 );
+
+const prompt = () => {
+    inquirer.prompt([{
+        type: "list",
+        message: "What would you like to do?",
+        name: "prompt",
+        choices: [
+            "View All Employees",
+            "Add Employee",
+            "Update Employee Role",
+            "View All Roles",
+            "Add Role",
+            "View All Departments",
+            "Add Department"
+
+        ]
+    }
+
+    ]).then(answer => {
+
+    }).catch(error => console.error(error));
+}
+
+
+
+
+prompt();
